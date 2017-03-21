30c8eaf14539b2b0f7c948b8d96d323e32168f07
tedsluis <ted.sluis@gmail.com>
2017-03-21 07:01:34 +0100

    put host facts in hash

--- nmapscan.pl
+++ nmapscan.pl
@@ -55,6 +55,7 @@ print $ipaddresses."\n";
 # Scan subnets
 my @node;
 my @link;
+my %host;
 my %hosts;    # hosts{ip} => ip+name
 my %ips;      # ips{subnet}{ip} => ip+name
 my %subnets;  # subnets{ip} => subnet
@@ -76,7 +77,7 @@ foreach my $subnet (@subnets) {
           } elsif ($line =~ /^\s*$/) {
                # Reached end of host info: start processing host info.
                print "IP ADDRESS=$ipaddress\n" if ($debug);
-	       $hosts{$ipaddress}=join(",",@key);
+               $hosts{$ipaddress}=join(",",@key);
                $ips{$subnet}{$ipaddress}=join(",",@key); 
                push(@node,"{ key: \"".join(",",@key)."\", desc: \"".join("\\n",@desc)."\", color: \"$color\", category: \"$category\" }");
                push(@link,"{ from: \"DEFAULTGATEWAY$subnet\", to: \"".join(",",@key)."\" }");
@@ -91,14 +92,17 @@ foreach my $subnet (@subnets) {
           } elsif ($line =~ /Nmap\sscan\sreport\sfor\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*/) {
                # parse IP address  
                $ipaddress=$1;
+               $host{$ipaddress}{'subnet'}=$subnet;
                $subnets{$ipaddress}=$subnet;
                push(@key,$1);
-	       #
+               #
                # Add gateway
                my @gw=split(/,/,$defaultgateway);
                for my $gw (@gw) {
-		    if ($gw =~ /^${ipaddress}$/) {
+                    if ($gw =~ /^${ipaddress}$/) {
+                         $host{$ipaddress}{'gateway'}=$ipaddress;
                          $gateway{$subnet}=$gw;
+                         $host{$ipaddress}{'color'}='red';
                          $color="red";
                          print "GATEWAY=$gw\n" if ($debug);
                     }
@@ -108,7 +112,9 @@ foreach my $subnet (@subnets) {
                     chomp($line);
                     # Parse hostname   
                     if ($line =~ /in-addr\.arpa\s+name\s=\s(.+)\.$/){
+                         $host{$ipaddress}{'hostname'}=$1;
                          push(@key,$1);
+                         $host{$ipaddress}{'color'}='lightblue' if (! exists $host{$ipaddress}{'color'});
                          $color="lightblue";
                          print "HOSTNAME=$1\n" if ($debug);
                     }
@@ -133,56 +139,71 @@ foreach my $subnet (@subnets) {
                push(@desc,"Route: ".join("-->",@route));
                next;
           } elsif ($line =~ /MAC\sAddress:\s([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})\s+\((.+)\)$/i) {
+               $host{$ipaddress}{'mac'}=$1;
                push(@desc,"MAC: $1");
+               $host{$ipaddress}{'vendor'}=$2;
                push(@desc,"Vendor: $2");
                print "MAC ADDRESS=$1 $2\n" if ($debug);
                next;
           } elsif ($line =~ /Host\sis\sup\s\((\d+\.\d+s\slatency)\)/) {
+               $host{$ipaddress}{'status'}='up';
                push(@desc,"Status: Up");
+               $host{$ipaddress}{'latency'}=$1;
                push(@desc,"Latency: $1");
                print "LATENCY=$1\n" if ($debug);
                next;
           } elsif ($line =~ /Network\sDistance:\s(\d+)\shop/) {
+               $host{$ipaddress}{'hops'}=$1;
                push(@desc,"Hops: $1");
                print "HOPS=$1\n" if ($debug);
                next;
           } elsif ($line =~ /Device type:\s(.+)$/) {
+               $host{$ipaddress}{'devicetype'}=$1;
                push(@desc,"Device type: $1");
                print "DEVICE TYPE=$1\n" if ($debug);
                next;
           } elsif ($line =~ /Running:\s(.+)$/) {
+               $host{$ipaddress}{'running'}=$1;
                push(@desc,"Running: $1");
                print "RUNNING=$1\n" if ($debug);
                next;
           } elsif ($line =~ /Not\sshown:\s(.+)$/) {
+               $host{$ipaddress}{'notshown'}.="$1\n";
                push(@desc,"Not shown: $1");
                print "NOT SHOWN=$1\n" if ($debug);       
                next;
           } elsif ($line =~ /OS\sCPE:\s(.+)$/) {
+               $host{$ipaddress}{'os'}.="$1\n";
                push(@desc,"OS CPE: $1");
                print "OS CPE=$1\n" if ($debug);
                next;
           } elsif ($line =~ /OS\sdetails:\s(.+)$/) {
+               $host{$ipaddress}{'os'}.="$1\n";
                push(@desc,"Os Details: $1");
                print "OS DETAILS=$1\n" if ($debug);
                next;
           } elsif ($line =~ /Warning:\s(.+)$/) {
+               $host{$ipaddress}{'warning'}.="$1.\n";
                push(@desc,"Warning: $1.");
                print "WARNING=$1\n" if ($debug);
                next;
           } elsif ($line =~ /(All\s1000\sscanned\sports)\son\s(.+ )(\sare\sclosed)/) {
+               $host{$ipaddress}{'notshown'}.="$1.$3\n";
                push(@desc,"Not shown: $1.$3");
                print "NOT SHOWN=$1.$3\n" if ($debug);
                next;
           } elsif ($line =~ /Aggressive OS guesses:\s(.+)$/) {
+               $host{$ipaddress}{'os'}.="$1\n";
                push(@desc,"Aggressive OS guesses: $1");
                print "AGGRESSIVE OS GUESSES=$1\n" if ($debug);
                next;
           } elsif ($line =~ /(Too\smany\sfingerprints.+)$/) {
+               $host{$ipaddress}{'warning'}.="$1.\n";
                push(@desc,"Warning: $1. ");
-               print "TOO MANY FINGERPRINTS=$1\n" if ($debug);
+               print "TOO MANY FINGERPRINTS=$1.\n" if ($debug);
                next;
           } elsif ($line =~ /(No\sexact\sOS\smatches.+)$/) {
+               $host{$ipaddress}{'warning'}.="$1.\n";
                push(@desc,"Warning: $1.");
                print "NO EXACT OS MATCHES=$1\n" if ($debug);
                next;
