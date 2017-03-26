cb56af4b4b3fb538b412a48945e48c341f52b187
tedsluis <ted.sluis@gmail.com>
2017-03-26 20:47:28 +0200

    Traceroute routine, help text, scan subnets

--- nmapscan.pl
+++ nmapscan.pl
@@ -9,27 +9,64 @@ my $debug;
 GetOptions(
      "help!"=>\$help,
      "debug!"=>\$debug,
-     "ipaddresses=s"=>\$ipaddresses
+     "subnets=s"=>\$ipaddresses
 ) or exit(1);
 #
 # Help
 if ($help) {
      print "Help
 
-usage: 
+Usage: 
        $0 
-optional:
-       $0 -ip <subnet>[,<subnet>]    Scan subnet(s).
+Optional:
+       $0 -subnet <cidr>[,<cidr>]    Subnet(s) in CIDR notation.
        $0 -debug                     Display debug info.
        $0 -help                      This helptext.
 
-examples:
+Examples:
        $0 -ip 192.168.1.0/24
        $0 -ip 192.168.1.0/24,192.168.11.0/24
 
-view result 'map.html' in a webbrowser.
+View result 'map.html' in a webbrowser.
 
-note: be sure 'nmap', 'traceroute' and 'network manager' are installed!\n\n";
+Lookup your Netbits:
++------+----------+------------------------------------+
+| Net  | Number   |                                    |
+| bits | of hosts |  Netmask                           |
++------+----------+------------------------------------+
+| /8   | 16777214 |  255.0.0.0                         |
+| /9   | 8388606  |  255.128.0.0                       |
+| /10  | 4194302  |  255.192.0.0                       |
+| /11  | 2097150  |  255.224.0.0                       |
+| /12  | 1048574  |  255.240.0.0                       |
+| /13  | 524286   |  255.248.0.0                       |
+| /14  | 262142   |  255.252.0.0                       |
+| /15  | 131070   |  255.254.0.0                       |
+| /16  | 65534    |  255.255.0.0                       |
+| /17  | 32766    |  255.255.128.0                     |
+| /18  | 16382    |  255.255.192.0                     |
+| /19  | 8190     |  255.255.224.0                     |
+| /20  | 4094     |  255.255.240.0                     |
+| /21  | 2046     |  255.255.248.0                     |
+| /22  | 1022     |  255.255.252.0                     |
+| /23  | 510      |  255.255.254.0                     |
+| /24  | 254      |  255.255.255.0                     |
+| /25  | 126      |  255.255.255.128                   |
+| /26  | 62       |  255.255.255.192                   |
+| /27  | 30       |  255.255.255.224                   |
+| /28  | 14       |  255.255.255.240                   |
+| /29  | 6        |  255.255.255.248                   |
+| /30  | 2        |  255.255.255.252                   |
+| /31  | -        |  point to point links only         |
+| /32  | 1        |  255.255.255.255 single IP address |
++------+----------+------------------------------------+
+Notes: 
+   - Number of hosts = usable number of hosts.
+   - CIDR = network address + /netbits, for example: 192.168.1.0/24
+   - Instead of the network address, any IP within the subnet is accepted.
+   - Be sure 'nmap', 'traceroute' and 'network manager' are installed!
+
+\n";
 exit 0;
 }
 
@@ -48,8 +85,6 @@ foreach my $hostip (@data) {
           push(@subnets,$1.$2) if (!$ipaddresses);
      }
 }
-$ipaddresses=join(",",@subnets) if (@subnets);
-print "Subnets to be scanned=$ipaddresses\n";
 
 #
 # default gateway
@@ -60,8 +95,44 @@ foreach my $line (@data) {
 }
 print "Host gateway=$hostgateway\n";
 
+#
+# Get CIDR, network address & broadcast address
+$ipaddresses=join(",",@subnets) if (@subnets);
+print "Subnets to be scanned:\n";
+my @ipaddresses=split(/,/,$ipaddresses);
+my %cidr;
+@subnets=();
+foreach my $subnet (@ipaddresses) {
+    # Validate CIDR
+    if ($subnet !~ /(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}$)/) {
+         print "CIDR '$1' is invalid!\n";
+         exit 1;
+    }
+    # 10.0.0.0/24 192.168.1.0/16
+    my($subnetwork, $netbit)=split(m'/',$subnet);
+    # Decimal representation of mask
+    my $mask  = (2 ** $netbit - 1) << (32 - $netbit); 
+    # Convert decimal representation to ip format
+    my $netmask = join( '.', unpack( "C4", pack( "N", $mask ) ) );
+    # Split to decimals
+    my ($ip1,$ip2,$ip3,$ip4)=($1,$2,$3,$4) if ($subnet  =~ /\s*(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\/\d{1,2}\s*/);
+    my ($ma1,$ma2,$ma3,$ma4)=($1,$2,$3,$4) if ($netmask =~ /\s*(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\s*/);
+    # Convert to bits
+    my ($ip)      = unpack( "N", pack( "C4", $ip1,$ip2,$ip3,$ip4 ) );
+    my ($mask)    = unpack( "N", pack( "C4", $ma1,$ma2,$ma3,$ma4 ) );
+    my ($fullmask)= unpack( "N", pack( "C4", 255,255,255,255 ) );
+    # Calculate network and broadcast, convert to ip format
+    my $network   = join( '.', unpack( "C4", pack( "N", ( $ip & $mask ) ) ) );
+    my $broadcast = join( '.', unpack( "C4", pack( "N", ( $ip | ($fullmask ^ $mask )) ) ) );
+    print "  $subnet (subnetwork=$subnetwork, netbit=$netbit, subnetmask=$netmask, network=$network broadcast=$broadcast)\n";
+    $cidr{"$subnetwork/$netbit"}{'subnetmask'}=$netmask;
+    $cidr{"$subnetwork/$netbit"}{'network'}=$network;
+    $cidr{"$subnetwork/$netbit"}{'broadcast'}=$broadcast;
+    push(@subnets,"$subnetwork/$netbit");
+}
+
 # 
-# Scan subnets
+# Initialize variables
 my @node;
 my @link;
 my %fact;
@@ -75,10 +146,53 @@ my %hosts;    # hosts{ip} => ip+name
 my %ips;      # ips{subnet}{ip} => ip+name
 my %subnets;  # subnets{ip} => subnet
 my %gateway;  # gateway{subnet} => ip gateway
-$gateway{'192.168.1.0/24'}='192.168.1.254';
-my @subnets=split(/,/,$ipaddresses);
+
+#
+# subroutine traceroute
+sub TraceRoute(@) {
+     my $ipaddress = shift;
+     my $subnet= shift;
+     my @route;
+     foreach my $hostip (keys %hostips) {
+          next if ((exists $interface{$hostip}) && (exists $interface{$hostip}{$subnet}) && ($interface{$hostip}{$subnet} =~ /UNREACHABLE/));
+          my @traceroute=`traceroute -i $hostips{$hostip} $ipaddress`;
+          print "traceroute -i $hostips{$hostip} $ipaddress " if ($debug);
+          foreach my $line (@traceroute) {
+               chomp($line);
+               if ($line =~ /^\s*\d+\s+\*\s+\*\s+\*/) {
+                    print "." if ($debug);
+                    $interface{$hostip}{$subnet}="UNREACHABLE" if (! exists $interface{$hostip}{$subnet});
+                    next;
+               }
+               print "\n" if ($debug);
+               # Parse route    
+               if ($line =~ /^\s*(\d+)\s+([a-z0-9\.\-]+)\s+\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)\s(.+)$/i) {
+                    my ($hop,$name,$ip,$rest)=($1,$2,$3,$4);
+                    $trace{$ipaddress}{$hop}=$ip;
+                    $reverse{$ipaddress}{$ip}=$hop;
+                    push(@route,"\[$name ($ip)\]");
+                    print "IPADDRESS=$ipaddress ---> HOP=$name $ip ($hop)  [via: interface=$hostips{$hostip} ($hostip)]\n" if ($debug);
+                    # Store gateway subnet.
+                    #if ($hop !~ /^1$/) {
+                    #     $gateway{$subnets{$ip}} = $ip if ((exists $subnets{$ip}) && (!exists $gateway{$subnets{$ip}}));
+                    #     if ($route[0] =~ /\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)/) {
+                    #           $gateway{$subnets{$1}} = $1 if ((exists $subnets{$1}) && (!exists $gateway{$subnets{$1}}));
+                    #     }
+                    # }
+                    # subnet is reachable using hostip
+                    $interface{$hostip}{$subnet}=$hostips{$hostip};
+                    $interfacesubnet{$subnet}=$hostip;
+               }
+          }
+     }    
+     print "\n".join("-->",@route)."\n" if ($debug);    
+     return @route;
+}
+
+#
+# Scan subnets
 foreach my $subnet (@subnets) {
-     print "SUBNET=$subnet\n" if ($debug);
+     print "Start scanning SUBNET=$subnet\n" if ($debug);
      my @data=`nmap -O -n $subnet`;
      my $info="";
      my @key;
@@ -110,7 +224,7 @@ foreach my $subnet (@subnets) {
                $ipaddress=$1;
                $host{$ipaddress}{'subnet'}=$subnet;
                $subnets{$ipaddress}=$subnet;
-	       print "SCAN HOST IP =$ipaddress, subnet=$subnet\n" if ($debug);
+               print "SCAN HOST IP =$ipaddress, subnet=$subnet\n" if ($debug);
                push(@key,$1);
                #
                # get hostname
@@ -126,41 +240,7 @@ foreach my $subnet (@subnets) {
                          print "HOSTNAME=$1\n" if ($debug);
                     }
                }
-               my @route;
-               foreach my $hostip (keys %hostips) {
-                    next if ((exists $interface{$hostip}) && (exists $interface{$hostip}{$subnet}) && ($interface{$hostip}{$subnet} =~ /UNREACHABLE/));
-                    my @traceroute=`traceroute -i $hostips{$hostip} $ipaddress`;
-                    print "traceroute -i $hostips{$hostip} $ipaddress " if ($debug);
-                    foreach my $line (@traceroute) {
-                         chomp($line);
-                         if ($line =~ /^\s*\d+\s+\*\s+\*\s+\*/) {
-                              print "." if ($debug);
-                              $interface{$hostip}{$subnet}="UNREACHABLE" if (! exists $interface{$hostip}{$subnet});
-                              next;
-                         }
-                         print "\n" if ($debug);
-                         # Parse route    
-                         if ($line =~ /^\s*(\d+)\s+([a-z0-9\.\-]+)\s+\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)\s(.+)$/i) {
-                              my ($hop,$name,$ip,$rest)=($1,$2,$3,$4);
-                              $trace{$ipaddress}{$hop}=$ip;
-                              $reverse{$ipaddress}{$ip}=$hop;
-                              push(@route,"\[$name ($ip)\]");
-                              print "IPADDRESS=$ipaddress ---> HOP=$name $ip ($hop)  [via: interface=$hostips{$hostip} ($hostip)]\n" if ($debug);
-                              # Store gateway subnet.
-			      #if ($hop !~ /^1$/) {
-			      #     $gateway{$subnets{$ip}} = $ip if ((exists $subnets{$ip}) && (!exists $gateway{$subnets{$ip}}));
-			      #     if ($route[0] =~ /\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)/) {
-			      #           $gateway{$subnets{$1}} = $1 if ((exists $subnets{$1}) && (!exists $gateway{$subnets{$1}}));
-			      #     }
-			      # }
-                              # subnet is reachable using hostip
-                              $interface{$hostip}{$subnet}=$hostips{$hostip};
-                              $interfacesubnet{$subnet}=$hostip;
-                         }
-                    }
-                    print "\n".join("-->",@route)."\n" if ($debug);    
-               }    
-               push(@desc,"Route: ".join("-->",@route));
+               push(@desc,"Route: ".join("-->",TraceRoute($ipaddress,$subnet)));
                next;
           } elsif ($line =~ /MAC\sAddress:\s([0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})\s+\((.+)\)$/i) {
                $host{$ipaddress}{'mac'}=$1;
@@ -233,6 +313,28 @@ foreach my $subnet (@subnets) {
                next;
           }
           $info.="$line\n";
+     }
+}
+
+#
+# Scan subnets
+foreach my $subnet (sort keys %cidr) {
+     my ($n1,$n2,$n3,$n4)=($1,$2,$3,$4) if ($cidr{$subnet}{'network'}   =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/);
+     my ($b1,$b2,$b3,$b4)=($1,$2,$3,$4) if ($cidr{$subnet}{'broadcast'} =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/);
+     for (my $ip1 = $n1; $ip1 <= $b1; $ip1++) {
+          for (my $ip2 = $n2; $ip2 <= $b2; $ip2++) {
+               for (my $ip3 = $n3; $ip3 <= $b3; $ip3++) {
+                    for (my $ip4 = $n4; $ip4 <= $b4; $ip4++) {
+                         my $ip="$ip1.$ip2.$ip3.$ip4";
+                         next if (($ip =~ /^$cidr{$subnet}{'network'}$/) || ($ip =~ /^$cidr{$subnet}{'broadcast'}$/)); 
+                         next if ((exists $subnets{$ip}) || (exists $cidr{$subnet}{'scanned_non_existing_ip'}));
+                         $subnets{$ip}=$subnet;
+                         my $route=join("-->",TraceRoute($ip,$subnet)); 
+                         $cidr{$subnet}{'scanned_non_existing_ip'}="yes";
+                         print "START=$cidr{$subnet}{'network'}, END=$cidr{$subnet}{'broadcast'}, IP=$ip, ROUTE=$route\n";
+                    }
+               }
+          }
      }
 }
 
